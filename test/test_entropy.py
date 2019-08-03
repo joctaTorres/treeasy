4909feccdcfa3da333343859af78e2f4721cf05c
Jocta <jocta.torres@gmail.com>
2019-08-02 23:56:37 -0300

    Adding test_collection_entropy_call

--- test/test_entropy.py
+++ test/test_entropy.py
@@ -1,7 +1,23 @@
 import pytest
+from unittest.mock import patch, call
 
-from treeasy.entropy import entropy
+from treeasy.entropy import entropy, collection_entropy
 
 def test_entropy(snapshot):
     snapshot.assert_match(entropy([(9/14), (5/14)]))
     snapshot.assert_match(entropy([0.5, 0.2, 0.1, 0.1, 0.1]))
+
+
+@patch("treeasy.entropy.entropy")
+def test_collection_entropy_call(entropy, snapshot):
+    collection_entropy([9, 5])
+    collection_entropy([7, 9, 4])
+
+    entropy.assert_has_calls(
+        [
+            call([(9/14), (5/14)]),
+            call([(7/20), (9/20), (4/20)])
+        ]
+    )
+
+
