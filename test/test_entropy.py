40ebf1653f54d7fe8df4ec9309db4d3fcbaebb63
Jocta <jocta.torres@gmail.com>
2019-08-03 00:13:50 -0300

    Make format

--- test/test_entropy.py
+++ test/test_entropy.py
@@ -1,10 +1,10 @@
-import pytest
-from unittest.mock import patch, call
+from unittest.mock import call, patch
 
-from treeasy.entropy import entropy, collection_entropy
+from treeasy.entropy import collection_entropy, entropy
+
 
 def test_entropy(snapshot):
-    snapshot.assert_match(entropy([(9/14), (5/14)]))
+    snapshot.assert_match(entropy([(9 / 14), (5 / 14)]))
     snapshot.assert_match(entropy([0.5, 0.2, 0.1, 0.1, 0.1]))
 
 
@@ -12,8 +12,8 @@ def test_collection_entropy(snapshot):
     collection_entropy_one = collection_entropy([9, 5])
     collection_entropy_two = collection_entropy([7, 9, 4])
 
-    entropy_one = entropy([(9/14), (5/14)])
-    entropy_two = entropy([(7/20), (9/20), (4/20)])
+    entropy_one = entropy([(9 / 14), (5 / 14)])
+    entropy_two = entropy([(7 / 20), (9 / 20), (4 / 20)])
 
     assert collection_entropy_one == entropy_one
     assert collection_entropy_two == entropy_two
@@ -28,8 +28,5 @@ def test_collection_entropy_call(entropy, snapshot):
     collection_entropy([7, 9, 4])
 
     entropy.assert_has_calls(
-        [
-            call([(9/14), (5/14)]),
-            call([(7/20), (9/20), (4/20)])
-        ]
+        [call([(9 / 14), (5 / 14)]), call([(7 / 20), (9 / 20), (4 / 20)])]
     )
--- treeasy/entropy.py
+++ treeasy/entropy.py
@@ -1,22 +1,23 @@
-from typing import List
 from math import log2
+from typing import List
 
-def collection_entropy(collection : List[int]) -> float:
-    '''
+
+def collection_entropy(collection: List[int]) -> float:
+    """
         collection:     discrete target attribute count
                         e.g.1: [7, 9, 4]
-    '''
+    """
 
     collection_size = sum(collection)
-    return entropy([(target/collection_size) for target in collection])
+    return entropy([(target / collection_size) for target in collection])
 
 
-def entropy(probabilities : List[float], log=log2) -> float:
-    '''
+def entropy(probabilities: List[float], log=log2) -> float:
+    """
         probabilities:  e.g.1: [(7/20), (9/20), (4/20)]
                         e.g.2: [(4/11), (7/11)]
                         e.g.3: [0.5, 0.2, 0.1, 0.1, 0.1]
-    '''
+    """
 
     if not probabilities:
         return 0
@@ -24,10 +25,7 @@ def entropy(probabilities : List[float], log=log2) -> float:
     entropy = 0
 
     for p in probabilities:
-        term = (p*log(p))
+        term = p * log(p)
         entropy -= term
-    
+
     return entropy
-
-
-
