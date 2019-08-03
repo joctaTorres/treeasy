de7c03b99e12a4a235a34eeeefdf957529def1a2
Jocta <jocta.torres@gmail.com>
2019-08-02 23:46:17 -0300

    Adding new function collection_entropy

--- treeasy/entropy.py
+++ treeasy/entropy.py
@@ -1,12 +1,25 @@
+from typing import List
 from math import log2
 
-def entropy(probabilities, log=log2):
+def collection_entropy(collection : List[int]):
     '''
-        probabilities: e.g [(7/20), (9/20), (4/20)] 
+        collection:     discrete target attribute count
+                        e.g.1: [7, 9, 4]
+    '''
+
+    collection_size = sum(collection)
+    return entropy([(target/collection_size) for target in collection])
+
+
+def entropy(probabilities : List[float], log=log2):
+    '''
+        probabilities:  e.g.1: [(7/20), (9/20), (4/20)]
+                        e.g.2: [(4/11), (7/11)]
+                        e.g.3: [0.5, 0.2, 0.1, 0.1, 0.1]
     '''
 
     if not probabilities:
-        return 1.0
+        return 0
 
     entropy = 0
 
